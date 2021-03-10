39acfde72d8233a139d65523c1fd03cd92cff8b5
Farah Ayu Khoirunnisa <71933404+farahayk@users.noreply.github.com>
2021-03-10 20:19:24 +0700

    Create Pertemuan_1.php

---
+++ Pertemuan_1.php
@@ -0,0 +1,73 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        <h2> Hello World </h2>
+    ?>
+</body>
+</html>
+
+
+<?php
+// Array
+// PHP ada array asosiatif, kalau di pemrograman lain dianggap objek
+// array asosiatif => array yang punya nama
+
+    $mhs = ]
+        [
+        'nama'=>'Farah'
+        'nim' => '192410101088'
+        'usia' =>'19'
+        ],
+        [
+        'nama'=>'Ayu'
+        'nim' => '192410101088'
+        'usia' =>'19'
+        ]
+        ];
+        //case menunujukan kondisi tertentu
+        //switch dan casae mirip dengan if
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Usia</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($arr as $mhs): ?>
+                <tr>
+                    <td> <?= $arr['nama']?></td>
+                    <td> <?= $arr ['nim']?></td>
+                    <td> <?= $arr ['mahasiswa']?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>    
+</body>
+</html>
+
+<?php
+// default aksi jika case-casenya tidak ketemu
+
+?>
